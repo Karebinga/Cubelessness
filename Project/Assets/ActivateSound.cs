59d70b769b0cad1a26959a6419d7eead3d4b0d7d
Karebinga <47193528+Karebinga@users.noreply.github.com>
2020-08-11 23:23:28 +0300

    Added Sound Obstacles

---
+++ Project/Assets/ActivateSound.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class ActivateSound : MonoBehaviour
+{
+    public GameObject player;
+    public float accuracy;
+    public AudioClip clip;
+
+    void FixedUpdate()
+    {
+        if (Mathf.Abs(gameObject.transform.position.z - player.transform.position.z) < accuracy)
+        {
+            AudioSource.PlayClipAtPoint(clip, gameObject.transform.position);
+        }
+    }
+}
