4cbeb06c656da83991b8411f2fa08a8a3f4368e1
Karebinga <47193528+Karebinga@users.noreply.github.com>
2020-08-13 23:41:50 +0300

    Added Pause, Settings, About

---
+++ Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pauseMenuUI;
+    public GameObject GameScreen;
+
+    public void Pause ()
+    {
+    pauseMenuUI.SetActive(true);
+        GameScreen.SetActive(false);
+    Time.timeScale = 0f;
+    }
+
+    public void Continue ()
+    {
+        pauseMenuUI.SetActive(false);
+        GameScreen.SetActive(true);
+        Time.timeScale = 1f;
+    }
+}
---
+++ Project/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenuUI;
+    public GameObject settingsMenuUI;
+    public GameObject aboutMenuUI;
+
+
+    public void Settings ()
+    {
+        pauseMenuUI.SetActive(false);
+        settingsMenuUI.SetActive(true);
+    }
+
+    public void Back ()
+    {
+        pauseMenuUI.SetActive(true);
+        settingsMenuUI.SetActive(false);
+    }
+
+    public void BackAbout()
+    {
+        aboutMenuUI.SetActive(false);
+        settingsMenuUI.SetActive(true);
+    }
+
+    public void About ()
+    {
+        aboutMenuUI.SetActive(true);
+        settingsMenuUI.SetActive(false);
+    }
+}
